47125a974044709aea268ba4c4c9ba2681630fb3
JeanPPG <jppenaranda@espe.edu.ec>
2025-02-19 18:52:21 -0500

    Fix Vite Tailwind deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: './', 
   preview: {
     // Asegura que el servidor se exponga en todas las interfaces
     host: true,
