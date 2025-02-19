c3f072ae8c73f4cff90a753d1213c25105ad0dcb
JeanPPG <jppenaranda@espe.edu.ec>
2025-02-19 18:37:07 -0500

    FIX HOST

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    // Asegura que el servidor se exponga en todas las interfaces
+    host: true,
+    // Agrega el host asignado por Railway a la lista de hosts permitidos
+    allowedHosts: ['interfaces-proyecto-r-production.up.railway.app']
+  }
 })
