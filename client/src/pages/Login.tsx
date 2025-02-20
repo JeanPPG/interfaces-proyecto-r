b1a6f12b917e1a638ec1a03cf963dbb58797f144
JEAN PIERRE PEÑARANDA GUEVARA <158861211+JeanPPG@users.noreply.github.com>
2025-02-20 07:49:55 -0500

    Update Login.tsx

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -19,7 +19,7 @@ export function Login() {
     console.log("Intentando iniciar sesión con:", { email, password });
 
     try {
-      const response = await fetch("/login", {  
+      const response = await fetch("https://interfaces-proyecto-r-production.up.railway.app//login", {  
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -35,7 +35,7 @@ export function Login() {
       if (!response.ok) throw new Error(data.message || "Error desconocido");
 
       login(email);
-      navigate("/testing");  // Redirige si la autenticación es correcta
+      navigate("https://interfaces-proyecto-r-production.up.railway.app/testing");  // Redirige si la autenticación es correcta
     } catch (error: any) {
       console.error("Error en la solicitud:", error);
       setError(error.message);
